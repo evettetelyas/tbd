f94c678615b83693135db385151a507c71fc3bd9
Tylor Schafer <tylorschafer@gmail.com>
2019-10-26 22:14:14 -0600

    Add more imports

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,19 +2,18 @@ import "core-js";
 import "regenerator-runtime/runtime";
 import 'bootstrap';
 import 'stylesheets/application.css';
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
 
-// require("@rails/ujs").start()
-// require("turbolinks").start()
-// require("@rails/activestorage").start();
+import 'jquery';
+import Rails from 'rails-ujs';
+import Turbolinks from 'turbolinks';
+
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require('core-js');
+
 require("channels");
-// require('core-js');
-// import 'jquery'
-// import Rails from 'rails-ujs'
-// import Turbolinks from 'turbolinks'
+
 
 // Rails.start()
 // Turbolinks.start()
