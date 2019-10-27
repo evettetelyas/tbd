c290745053dbd998a4bca0d3363a651e609b322c
Tylor Schafer <tylorschafer@gmail.com>
2019-10-26 22:24:33 -0600

    Remove turbolink and rails referencess in app.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,13 +7,13 @@ import 'jquery';
 import Rails from 'rails-ujs';
 import Turbolinks from 'turbolinks';
 
-require("@rails/ujs").start();
-require("turbolinks").start();
-require("@rails/activestorage").start();
 require('core-js');
-
-// Rails.start()
-// Turbolinks.start()
+// require("@rails/ujs").start();
+// require("turbolinks").start();
+// require("@rails/activestorage").start();
+//
+// Rails.start();
+// Turbolinks.start();
 
 require("channels");
 
