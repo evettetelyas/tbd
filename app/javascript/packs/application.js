a073bdb7ab8c51cbb48ce174f9e59fcbf1050448
Tylor Schafer <tylorschafer@gmail.com>
2019-10-26 23:05:29 -0600

    Comment out app.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,22 +1,22 @@
-import "core-js";
-import "regenerator-runtime/runtime";
-import 'bootstrap';
-import '/app/javascript/stylesheets/application';
-import '/app/assets/stylesheets';
+// import "core-js";
+// import 'jquery';
+// import "regenerator-runtime/runtime";
+// import 'bootstrap';
+// import 'stylesheets/application';
+// // import '/app/assets/stylesheets';
+//
+// // import Rails from 'rails-ujs';
+// // import Turbolinks from 'turbolinks';
+//
+// require("channels");
+// require('core-js');
 
-import 'jquery';
-// import Rails from 'rails-ujs';
-// import Turbolinks from 'turbolinks';
-
-require('core-js');
 // require("@rails/ujs").start();
 // require("turbolinks").start();
 // require("@rails/activestorage").start();
-//
 // Rails.start();
 // Turbolinks.start();
 
-require("channels");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
