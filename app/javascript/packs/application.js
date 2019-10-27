7805e5a19fc4d734cd2f76dc247bc6ac640144e5
Tylor Schafer <tylorschafer@gmail.com>
2019-10-26 20:44:39 -0600

    Comment out app.js lines

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import '../../assets/stylesheets/application.css'
-import 'jquery'
+// import '../../assets/stylesheets/application.css'
+// import 'jquery'
 import 'bootstrap'
 import Rails from 'rails-ujs'
 import Turbolinks from 'turbolinks'
