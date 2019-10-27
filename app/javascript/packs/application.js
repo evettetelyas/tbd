673fa00bc448c71ed4c979719a86848a5ef381c9
Tylor Schafer <tylorschafer@gmail.com>
2019-10-26 20:31:26 -0600

    Remove images line from app.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
 import '../stylesheets/application.css'
 import 'jquery'
 import 'bootstrap'
