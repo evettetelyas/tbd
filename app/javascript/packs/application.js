d16e40926af54b83d9170ccaa6b968cca9ac6842
Tylor Schafer <tylorschafer@gmail.com>
2019-10-26 20:35:56 -0600

    Change routes in app.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import '../stylesheets/application.css'
+import '../../assets/stylesheets/application.css'
 import 'jquery'
 import 'bootstrap'
 import Rails from 'rails-ujs'
