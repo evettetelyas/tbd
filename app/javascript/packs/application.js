462de792c12144c804c18b868279d5d058c9ac98
Tylor Schafer <tylorschafer@gmail.com>
2019-10-26 20:27:29 -0600

    Lugo labs tutorial

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,15 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import '../assets/stylesheets/application.css'
+require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
+import '../stylesheets/application.css'
+import 'jquery'
+import 'bootstrap'
+import Rails from 'rails-ujs'
+import Turbolinks from 'turbolinks'
 
+Rails.start()
+Turbolinks.start()
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
